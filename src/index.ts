535911919181f4ebcb47ad008f6558c920d015dd
matzryo <matzryo@gmail.com>
2023-02-16 17:53:34 +0900

    TypeScript化

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import nameCorrespondence from "../data/ONE.json" assert { type: "json" };
 
-var writeTogether = ({ nameEn, nameJa }) => [nameEn, nameJa].join(" / ");
+var writeTogether = ({ nameEn, nameJa }: { nameEn: string; nameJa: string }) =>
+  [nameEn, nameJa].join(" / ");
 
-var cardNameCells = document.querySelectorAll("table .list_card_name");
+var cardNameCells = document.querySelectorAll<HTMLDivElement>(
+  "table .list_card_name"
+);
 
 nameCorrespondence.data.forEach((names) => {
   cardNameCells.forEach((cell) => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: 'ts-loader',
+      },
+    ],
+  },
+  // import 文で .ts ファイルを解決するため
+  // これを定義しないと import 文で拡張子を書く必要が生まれる。
+  // フロントエンドの開発では拡張子を省略することが多いので、
+  // 記載したほうがトラブルに巻き込まれにくい。
+  resolve: {
+    // 拡張子を配列で指定
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
